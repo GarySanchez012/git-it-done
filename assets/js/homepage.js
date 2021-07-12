c2dd2f5b47f6986edcbcb9bb9d2581380647e9e0
arroyhondo <garysanchez012@gmail.com>
2021-07-12 17:10:55 -0400

    i forgot to create a new branch and work on htat rather than work on the develop branch but i added in anothr card so that a user can look for JS, HTML, or CSS repos with issues

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -17,27 +18,55 @@ var formSubmitHandler = function (event) {
 };
 
 var getUserRepos = function (user) {
-  //format the github api url
-  var apiUrl = "https://api.github.com/users/" + user + "/repos";
-
-  //make a request to the url
-  fetch(apiUrl)
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+    
+    //make a request to the url
+    fetch(apiUrl)
     .then(function (response) {
-      //request was successful
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data, user);
-        });
-      } else {
-        alert("Error: GitHub User Not Found");
-      }
+        //request was successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
     })
     .catch(function (error) {
-      //Notice this '.catch()' getting chained onto the end of the '.then()'
-      alert("Unable to connect to GitHub");
+        //Notice this '.catch()' getting chained onto the end of the '.then()'
+        alert("Unable to connect to GitHub");
     });
 };
 
+var buttonClickHandler = function (event) {
+  var language = event.target.getAttribute("data-language");
+  
+  if (language) {
+      getFeaturedRepos(language);
+
+      //clear old content
+      repoContainerEl.textContent = "";
+  }
+};
+
+var getFeaturedRepos = function (language) {
+  var apiUrl =
+    "https://api.github.com/search/repositories?q=" +
+    language +
+    "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: GitHub User Not Found");
+    }
+  });
+};
+ 
 var displayRepos = function (repos, searchTerm) {
   //check if api returned any repos
   if (repos.length === 0) {
@@ -91,3 +120,4 @@ var displayRepos = function (repos, searchTerm) {
 };
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
